023109fbcb4108408710efdc3bc316262bdca4ac
Martin R. Vasilev <martin.r.vasilev@gmail.com>
2019-04-20 21:19:51 +0100

    some results

--- Data_analysis.R
+++ Data_analysis.R
@@ -66,5 +66,60 @@ SGM1<- coef(summary(GLM1))
 SGM1
 
 
+###########################
+# FIXATION DURATION DATA: #
+###########################
+
+load("data/sound_check.Rda")
+
+DesFix<- melt(sound_check, id=c('sub', 'item', 'cond', 'sound_type', 'del'), 
+              measure=c("N1"), na.rm=TRUE)
+mFix<- cast(DesFix, sound_type+del ~ variable
+            ,function(x) c(M=signif(mean(x),3)
+                           , SD= sd(x) ))
+
+colnames(mFix)<- c("Sound", "Delay", "Mean", "SD")
+mFix$SE<- mFix$SD/sqrt(64)
+mFix$Delay<- as.factor(mFix$Delay)
+mFix$Sound<- as.factor(mFix$Sound)
+levels(mFix$Sound)<- c("Novel", "Standard")
+
+Plot <-ggplot(mFix, aes(x= Delay, y= Mean, group= Sound, colour= Sound, shape= Sound,
+                        ymin= Mean- SE, ymax= Mean+SE)) +
+  theme_bw(16) +geom_point(size=5)+ geom_line(size=2)+ 
+  scale_colour_brewer(palette="Accent")+ xlab("Sound onset delay (in ms)")+
+  ylab("First fixation duration (in ms)")+ geom_ribbon(alpha=0.1, colour=NA)+
+  theme(legend.position="bottom", legend.key.width=unit(1.5,"cm")); Plot
+
+ggsave(Plot, filename = "Plots/FFD_mainFig.pdf", width = 5, height = 5)
+
+
+###### LMM analysis:
+
+# Note, for historical reasons (i.e., ease of programming) I'm still using "DEV" to denote the different (deviant) sound.
+# However, in this experiment a novel sound was used, so "DEV"= Novel.
+
+# check contrast coding
+sound_check$sound_type<- as.factor(sound_check$sound_type)
+sound_check$sound_type<- factor(sound_check$sound_type, levels= c("STD", "DEV"))
+contrasts(sound_check$sound_type)#<- c(1, 0)
+
+
+sound_check$del<- as.factor(sound_check$del)
+contrasts(sound_check$del)
+
+if(!file.exists("Models/LM1.Rda")){
+  LM1<-lmer(log(N1) ~ sound_type*del + (sound_type+del|sub)+ (sound_type+del|item),
+            data= sound_check)
+  save(LM1, file= "Models/LM1.Rda")
+}else{
+  load("Models/LM1.Rda")
+}
+
+SM1<- round(coef(summary(LM1)),3)
+
+rownames(SM1)<- c("Intercept", "Sound (Novel vs STD)", "Delay (120 vs 0ms)", "Sound x Delay")
+write.csv2(SM1, file = "Models/")
+
 
 
